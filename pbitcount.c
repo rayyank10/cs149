84f3ca4350bf78f88d469c16b2b4af6c38260714
rayyank10 <57589073+rayyank10@users.noreply.github.com>
2020-09-20 23:50:47 -0700

    Update pbitcount.c

--- pbitcount.c
+++ pbitcount.c
@@ -1,141 +1,71 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
-
 int countSetBits(int n) {
-
-  //intialize a count variable
-
-  int count = 0;
-
-  //while n is greater than zero use & on n-1 and set that value to n
-
-  while (n > 0) {
-
-    n &= (n - 1);
-
-    //add one to count
-
-    count = count + 1;
-
-  }
-
-  return count;
-
+    //intialize a count variable
+    int count = 0;
+    //while n is greater than zero use & on n-1 and set that value to n
+    while (n > 0) {
+        n &= (n - 1);
+        //add one to count
+        count = count + 1;
+    }
+    return count;
 }
 
-int main(int argc, char ** argv) {
-
-  int Bitcount(char * file);
-
-  int bit = 0;
-
-  int totalbits = 0;
-
-  int pid;
-
-  int fds[2];
-
-  pipe(fds);
-
-  //check to see if there is an appropriate amount of arguments
-
-  if (argc >= 3) {
-
-    for (int i = 1; i < argc; i++) {
-
-      pid = fork();
-
-      //check to see if its the child process
-
-      //Then check to see if the fork fails
-
-      if (pid == 0) {
-
-        totalbits += Bitcount(argv[i]);
-
-        write(fds[1], & totalbits, sizeof(totalbits));
-
-        return 0;
-
-      } else if (pid < 0) {
-
-        printf("Fork Failed");
-
-        _exit(3);
-
-      }
-
+int main(int argc, char **argv) {
+    int Bitcount(char *file);
+    int pid;
+    int fds[2];
+    pipe(fds);
+    int bit = 0;
+    int totalbits = 0;
+    if (argc >= 3) {
+        for (int i = 1; i < argc; i++) {
+            pid = fork();
+            if (pid == 0) {
+                totalbits += Bitcount(argv[i]);
+                write(fds[1], &totalbits, sizeof(totalbits));
+                _exit(0);
+            } else if (pid < 0) {
+                printf("Fork Failed");
+                _exit(3);
+            }
+        }
+        // Parent process
+        for (int i = 1; i < argc; i++) {
+            read(fds[0], &totalbits, sizeof(totalbits));
+            bit += totalbits;
+        }
+        printf("%i\n", bit);
+         _exit(0);
     }
-
-    // Parent process
-
-    for (int i = 1; i < argc; i++) {
-
-      read(fds[0], & totalbits, sizeof(totalbits));
-
-      bit += totalbits;
-
+        else if (argc <= 2) {
+            printf("USAGE: %s  filename \n", argv[1]);
+            printf("%s %s \n", argv[0], argv[1]);
+            printf("$ echo $?\n");
+            _exit(1);
     }
-
-    printf("%i\n", bit);
-
-    return 0;
-
-  }
-
-  //if less than 3 arguments print usage statements
-  else if (argc <= 2) {
-
-    printf("USAGE: %s  filename \n", argv[1]);
-
-    printf("%s %s \n", argv[0], argv[1]);
-
-    printf("$ echo $?\n");
-
-    _exit(1);
-
-  }
-
 }
 
-int Bitcount(char * file) {
+int Bitcount(char* file){
 
-  char * filename = file;
-
-  FILE * fh;
-
-  fh = fopen(filename, "r");
-
-  if (fh == NULL) {
-
-    perror(file);
-
-    printf("$ echo $?\n");
-
-    _exit(2);
-
-  }
-
-  //initialize the ch variable and the ab variable
-
-  int ch = fgetc(fh);
-
-  int bit = 0;
-
-  //while ch doesnt reach the end of file keep getting each integer and counting the set bits and add them to the bit variable
-
-  while (ch != EOF) {
-
-    bit = bit + countSetBits(ch);
-
-    ch = fgetc(fh);
-
-  }
-
-  fclose(fh);
-
-  return bit;
-
+    char *filename = file;
+    FILE *fh;
+    fh = fopen(filename, "r");
+    if(!fh){
+        perror(file);
+        printf("$ echo $?\n");
+        _exit(2);
+    }
+    //initialize the ch variable and the ab variable
+    int ch = fgetc(fh);
+    int bit = 0;
+    //while ch doesnt reach the end of file keep getting each integer and counting the set bits and add them to the bit variable
+    while (ch != EOF) {
+        bit = bit + countSetBits(ch);
+        ch = fgetc(fh);
+    }
+    fclose(fh);
+    return bit;
 }
