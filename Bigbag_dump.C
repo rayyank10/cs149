b4976f0da42207468abd8904b03330f1b1fe1a3b
rayyank10 <57589073+rayyank10@users.noreply.github.com>
2020-11-01 17:37:30 -0800

    Update Bigbag_dump.C

--- Bigbag_dump.C
+++ Bigbag_dump.C
@@ -26,48 +26,75 @@ uint32_t entry_offset(void *hdr, void *entry) {
 
 
 int main(int argc, char **argv) {
-    void *base;
+   int fd;
+    void *file_base;
 
-    // Opens the file
-    int fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        perror(argv[1]);
-        return 2;
+    if( argc == 2) {                   // here we check if user wants to enter protected mode
+        fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        if (fd == -1) {
+            perror(argv[1]);
+            exit(3);
+        }
+        else{
+            ftruncate(fd,BIGBAG_SIZE);
+            file_base = mmap(0, BIGBAG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        }
+    }else if (strcmp(argv[1],"-t") == 0  && argc == 3){
+        fd = open(argv[2], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        if (fd == -1) {
+            perror(argv[2]);
+            exit(3);
+        }
+        else {
+            ftruncate(fd,BIGBAG_SIZE);
+            file_base = mmap(0, BIGBAG_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+        }
+
     }
-    //base = mmap(NULL, 1024*1024, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
-
-    // Maps the file
-    void *file_base = mmap(0, BIGBAG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (file_base == MAP_FAILED) {
+    else
+    {
+        printf("USAGE: %s bos_filename or %s -t bos-filename\n", argv[0],argv[0]);
+        exit(2);
+    }
+  if (file_base == MAP_FAILED) {
         perror("mmap");
         return 3;
     }
-    void *end;
 
     // Header is now the file
     struct bigbag_hdr_s *hdr = static_cast<bigbag_hdr_s *>(file_base);
-    struct bigbag_entry_s *first_entry;
+    struct bigbag_entry_s *first_entry = reinterpret_cast<bigbag_entry_s *>(static_cast<bigbag_hdr_s *>(file_base) +
+
+                                                                            hdr->first_free);
+    int newFile = 0;
+    if(hdr->first_free == 0)
+    {
+        newFile =1;
+    }
+    if(newFile)
+    {
+        hdr->magic = 0xA99B15C5;
+        hdr->first_free = 12;
+        hdr->first_element = 12;
+    }
+    if(newFile)
+    {
+        first_entry->next = 0;
+        first_entry->entry_magic = BIGBAG_FREE_ENTRY_MAGIC;
+        first_entry->entry_len = BIGBAG_SIZE-MIN_ENTRY_SIZE-12;
+    }
+    entry_addr(hdr,hdr->first_free)->next = 0;
     struct stat stat;
     fstat(fd, &stat);
     size_t n;
     char *line = NULL;
-
     while (getline(&line, &n, stdin) > 0) {
         // remove the newline
         size_t endi = strlen(line) - 1;
         if (line[endi] == '\n') {
             line[endi] = '\0';
         }
-        switch (line[0]) {
-
-            case 'l': {
-
-                // This part begins listing out all the entries in the file
-                printf("size = %ld\n", stat.st_size);
-                printf("magic = %08x\n", htonl(hdr->magic));
-                printf("first_free = %d\n", hdr->first_free);
-                printf("first_element = %d\n", hdr->first_element);
-
+      if (line[0] == 'l' ) {
                 struct bigbag_entry_s *entry;
                 int offset = hdr->first_element; // Offset is choosing the entries
                 while (offset + sizeof(*entry) < stat.st_size) {
@@ -77,33 +104,87 @@ int main(int argc, char **argv) {
                         printf("bad entry at offset %d\n", offset);
                         break;
                     }
-                    printf("----------------\n");
-                    printf("entry offset: %d\n", offset); // Location of data
-                    printf("entry magic: %x\n",
-                           (int) entry->entry_magic); // Magic is either 0xFA or 0xDA, meaning free space or used space
-                    printf("entry len: %d\n", entry->entry_len); // Length of data
-                    printf("entry next offset: %d\n", entry->next); // Next entry, notice how it isn't printing by next.
                     if (entry->entry_magic == BIGBAG_USED_ENTRY_MAGIC) { // Only print string if it is a used space
-                        printf("entry data: %s\n", entry->str);
+                        printf("%s\n", entry->str);
                     }
 
                     offset = entry->next;
                 }
-                break;
             }
-            case 'a': {
+            else if (line[0] == 'a') {
+
                 struct bigbag_entry_s *bestFitEntry;
-                int canFit = 0;
-                char *str = &line[1];
+                struct bigbag_entry_s *prevEntry;
+                struct bigbag_entry_s *currentEntry;
+
+                currentEntry = entry_addr(hdr,hdr->first_element);
+                char *str = &line[2];
+                int stringSize = strlen(str)+1;
                 int offset = hdr->first_free;
+              hdr->first_free = hdr->first_free + MIN_ENTRY_SIZE + stringSize;
+                struct bigbag_entry_s *freeSpace;
+                freeSpace = entry_addr(hdr,hdr->first_free);
+                freeSpace->next = 0;
+                freeSpace->entry_magic = BIGBAG_FREE_ENTRY_MAGIC;
                 bestFitEntry= entry_addr(hdr, offset);
-               printf(str);
-                strcpy(bestFitEntry->str,str);
+                freeSpace->entry_len = freeSpace->entry_len - stringSize - MIN_ENTRY_SIZE;
+                bestFitEntry->entry_magic = BIGBAG_USED_ENTRY_MAGIC;
+                bestFitEntry->entry_len = stringSize;
+               if(freeSpace->entry_len > stringSize + MIN_ENTRY_SIZE) {
+                   strcpy(bestFitEntry->str, str);
 
-
+                   while (currentEntry->next != 0) {
+                       if (strcmp(str, currentEntry->str) < 0) {
+                           break;
+                       }
+                       prevEntry = currentEntry;
+                       currentEntry = entry_addr(hdr, currentEntry->next);
+                   }
+                   if (entry_offset(hdr, currentEntry) != hdr->first_element) {
+                       prevEntry->next = entry_offset(hdr, bestFitEntry);
+                   } else {
+                       hdr->first_element = entry_offset(hdr, bestFitEntry);
+                   }
+                   bestFitEntry->next = entry_offset(hdr, currentEntry);
+                   printf("added %s", str);
+                   break;
+               }
+               else {
+                   printf("out of space");
+               }
             }
+            else if (line[0] == 'c')
+            {
+                struct bigbag_entry_s *ctEntry;
+                ctEntry = entry_addr(hdr,hdr->first_element);
+                char *str = &line[2];
+                int check;
+                while (ctEntry->next != 0)
+                {
+                    if(strcmp(str, ctEntry->str)==0){
+                        printf("This string is already here");
+                        check = 0;
+
+                        break;
+                    }
+                    else{
+                        check =1;
+                    }
+                    ctEntry = entry_addr(hdr,ctEntry->next);
+
+                }
+                if(check ==1)
+                {
+                    printf("not here");
+                }
+            }
+            else
+      {
+
+                printf("%s not used correctly", line);
+                printf("\n Possible Commands \n a:string_to_add \n c:string_to_check \n l");
+      }
+
         }
 
     }
-
-}
