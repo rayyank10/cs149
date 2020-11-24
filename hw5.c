5ff8fccc146d1f82e8b26a48fd20bb0aecebb963
rayyank10 <57589073+rayyank10@users.noreply.github.com>
2020-11-23 21:59:22 -0800

    Update hw5.c

--- hw5.c
+++ hw5.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+
+//global variable to keep track of count of unique words
 int count;
 
 typedef struct __node_t {
-    char* key;
+    char *key;
     struct __node_t *next;
 } node_t;
 
@@ -20,7 +22,7 @@ void List_Init(list_t *L) {
     pthread_mutex_init(&L->lock, NULL);
 }
 
-int List_Insert(list_t *L, char* key) {
+int List_Insert(list_t *L, char *key) {
     // synchronization not needed
     node_t *new = malloc(sizeof(node_t));
     if (new == NULL) {
@@ -35,22 +37,24 @@ int List_Insert(list_t *L, char* key) {
 
     pthread_mutex_unlock(&L->lock);
 }
-int List_Lookup(list_t *L, char* key) {
+
+int List_Lookup(list_t *L, char *key) {
     int rv = -1;
 
     pthread_mutex_lock(&L->lock);
     node_t *curr = L->head;
     while (curr) {
-        if (strcmp(curr->key,key)==0) {
+        // changed from the book due to it being more accurate
+        if (strcmp(curr->key, key) == 0) {
             rv = 0;
             break;
         }
         curr = curr->next;
     }
     pthread_mutex_unlock(&L->lock);
-    return rv; // now both success and failure
+    return rv;
 }
-
+//set buckets to equal 256
 #define BUCKETS (256)
 typedef struct __hash_t {
     list_t lists[BUCKETS];
@@ -58,8 +62,7 @@ typedef struct __hash_t {
 
 //taken from stackoverflow
 unsigned long
-hash(unsigned char *str)
-{
+hash(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
 
@@ -68,43 +71,41 @@ hash(unsigned char *str)
 
     return hash;
 }
+
 void Hash_Init(hash_t *H) {
     int i;
     for (i = 0; i < BUCKETS; i++)
         List_Init(&H->lists[i]);
 }
 
-int Hash_Insert(hash_t *H, char* key) {
+int Hash_Insert(hash_t *H, char *key) {
     return List_Insert(&H->lists[hash(key) % BUCKETS], key);
 }
 
-int Hash_Lookup(hash_t *H, char* key) {
+int Hash_Lookup(hash_t *H, char *key) {
     return List_Lookup(&H->lists[hash(key) % BUCKETS], key);
 }
 
-struct _args
-{
+struct _args {
     char *argv;
-    struct hash_t *hash ;
+    struct hash_t *hash;
 };
 pthread_mutex_t lock;
 
-void *unique_Words(void *v)
-{
-    struct _args *args  = v;
+void *unique_Words(void *v) {
+    struct _args *args = v;
     char *word;
     FILE *file;
-    file = fopen(args->argv,"r");
-    if(file==NULL)
-    {
+    file = fopen(args->argv, "r");
+    if (file == NULL) {
         perror(args->argv);
-        exit(2);
+        //exit(2);
+        return 0;
     }
-    while (fscanf(file, "%ms", &word) != EOF)
-    {
+    while (fscanf(file, "%ms", &word) != EOF) {
         pthread_mutex_lock(&lock);
-        if(Hash_Lookup(args->hash,word)){
-            Hash_Insert(args->hash,word);
+        if (Hash_Lookup(args->hash, word)) {
+            Hash_Insert(args->hash, word);
 
         }
         pthread_mutex_unlock(&lock);
@@ -114,23 +115,20 @@ void *unique_Words(void *v)
 }
 
 
-
-
 int main(int argc, char **argv) {
     struct _args arguments;
+    //initializing the hash and allocating space for it
     hash_t *hash1 = malloc(sizeof(hash_t));
     Hash_Init(hash1);
     arguments.hash = hash1;
     int size = argc;
     pthread_t tid[argc];
-    pthread_mutex_init(&lock,NULL);
 
-    for(int i = 1; i<size; i++)
-{
+    for (int i = 1; i < size; i++) {
         arguments.argv = argv[i];
-       pthread_create(&tid[i],NULL,unique_Words,&arguments);
-       pthread_join(tid[i],NULL);
-}
+        pthread_create(&tid[i], NULL, unique_Words, &arguments);
+        pthread_join(tid[i], NULL);
+    }
 
-    printf("%d \n",count);
+    printf("%d \n", count);
 }
